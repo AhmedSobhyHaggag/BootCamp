9bbd3bdaafeefd48c9c54ba87c14abad0f2464aa
AhmedSobhyHaggag <ahmed.sobhy@avelabs.com>
2019-02-17 18:07:01 +0200

    Revert "story8"
    This reverts commit f1666969c3c710dfebe81f5c80ee17fbb10377d4.

--- GccApplication4/GccApplication4/Debug/Memory_Mapped_Register.d
+++
@@ -1,9 +0,0 @@
-Memory_Mapped_Register.d Memory_Mapped_Register.o: \
- .././Memory_Mapped_Register.c .././Types.h .././BitwiseOperations.h \
- .././Memory_Mapped_Register\ .h
-
-.././Types.h:
-
-.././BitwiseOperations.h:
-
-.././Memory_Mapped_Register\ .h:
